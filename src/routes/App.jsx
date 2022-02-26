4194c4475d23cd9b5e88e7bafe605de4712bd0f9
victorraul <victor-raul-22@hotmail.com>
2022-02-26 13:59:27 -0500

    test css

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from '@containers/Home';
-import Checkout from '@containers/Checkout';
-import Information from '@containers/Information';
-import Payment from '@containers/Payment';
-import Success from '@containers/Success';
-import NotFound from '@containers/NotFound';
-import Layout from '@components/Layout';
-import useInitialState from '@hooks/useInitialState';
-import Appcontext from '../context/AppContext';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "@containers/Home";
+import Checkout from "@containers/Checkout";
+import Information from "@containers/Information";
+import Payment from "@containers/Payment";
+import Success from "@containers/Success";
+import NotFound from "@containers/NotFound";
+import Layout from "@components/Layout";
+import useInitialState from "@hooks/useInitialState";
+import Appcontext from "../context/AppContext";
 
 function App() {
   const initialState = useInitialState();

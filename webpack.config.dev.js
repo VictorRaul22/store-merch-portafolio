4153188616512e152d1ffeae84e7bff4daa8db54
victorraul <victor-raul-22@hotmail.com>
2022-02-28 04:31:52 -0500

    add webpack config dev

---
+++ webpack.config.dev.js
@@ -0,0 +1,95 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require('webpack');
+require('dotenv').config();
+const Dotenv = require('dotenv-webpack');
+
+/** @type {import('webpack').Configuration} */
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "production",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/bundle.js",
+    publicPath: "/",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      "@containers": path.resolve(__dirname, "src/containers"),
+      "@context": path.resolve(__dirname, "src/context"),
+      "@routes": path.resolve(__dirname, "src/routes"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+    },
+  },
+  module: {
+    rules: [
+      // trabajar con babel
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      // html
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        type: "asset",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+    }),
+    new Dotenv({
+      path: './.env',
+      safe: true,
+      systemvars: true,
+      defaults: false
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'CLIENT_ID_PP': JSON.stringify(process.env.CLIENT_ID_PP),
+        'MAP_API_KEY': JSON.stringify(process.env.MAP_API_KEY),
+        'MAP_JS_API_KEY': JSON.stringify(process.env.MAP_JS_API_KEY),
+
+      }
+    })
+  ],
+  devServer: {
+    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 3000,
+  },
+};

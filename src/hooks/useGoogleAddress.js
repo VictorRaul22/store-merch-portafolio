a573fabcd169a9439a77b4fd8d91c8e21fcb7965
victorraul <victor-raul-22@hotmail.com>
2022-03-05 16:29:02 -0500

    elimate console.log

--- src/hooks/useGoogleAddress.js
+++ src/hooks/useGoogleAddress.js
@@ -6,20 +6,20 @@ const useGoogleAdress = (address) => {
   const API = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
     address
   )}&key=${pass.mapApiKey}`;
-  console.log(pass.mapApiKey);
-  window.console.log(API);
+  // console.log(pass.mapApiKey);
+  // window.console.log(API);
   useEffect(() => {
     const fetchMap = async () => {
       const result = await fetch(API);
-      console.log(result);
+      // console.log(result);
       if (!result.ok) throw new Error(Promise.reject(result));
       const json = await result.json();
-      console.log(json);
+      // console.log(json);
       setMap(json.results[0].geometry.location);
     };
     fetchMap();
   }, [API]);
-  window.console.log(map);
+  // window.console.log(map);
   return map;
 };
 export default useGoogleAdress;
